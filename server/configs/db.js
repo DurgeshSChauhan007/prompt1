39f9a26d1f82b540327f353ab5bb50c977ceeb00
DurgeshSChauhan007 <durgeshsinghchauhan97.4@gmail.com>
2026-03-27 14:17:52 +0530

    commit db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,14 +1,22 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-    try {
-        mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/car-rental`)
+const MONGODB_URI = process.env.MONGODB_URI;
 
-        console.log("ENV:", process.env.MONGODB_URI);
-    } catch (error) {
-        console.log(error.message);
-    }
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI");
 }
+
+let cached = global.mongoose || { conn: null, promise: null };
+
+const connectDB = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI + "/car-rental");
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
 
 export default connectDB;
